8f42accd6d9e91986cbc2bd93f32931fa3ab8410
heydabop <heydabop@gmail.com>
2014-11-05 12:20:35 -0600

    exec sqlite3 INSERT statement

--- addDB/addDB.go
+++ addDB/addDB.go
@@ -1,8 +1,8 @@
 package main
 
 import(
-	_ "github.com/mattn/go-sqlite3"
-	_ "database/sql"
+	"github.com/mattn/go-sqlite3"
+	"database/sql"
 	"bufio"
 	"fmt"
 	"log"
@@ -19,16 +19,23 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer txtFile.Close()
+	db, err := sql.Open("sqlite3", "../classes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	txtReader := bufio.NewReader(txtFile)
 	line := ""
 	for line, err = txtReader.ReadString('\n'); err == nil; line, err = txtReader.ReadString('\n') {
 		if match := classLineRegex.FindStringSubmatch(line); match != nil {
-			for i := 1; i < len(match); i++ {
-				fmt.Printf("%s ", match[i])
+			/*for i := 1; i < len(match); i++ {
+				fmt.Printf("%s, ", match[i])
 			}
-			fmt.Println()
-			/*dept := match[1]
+			fmt.Println()*/
+			dept := match[1]
 			classNo := match[2]
+			deptClassNo := match[1] + "-" + match[2]
 			A := match[3]
 			B := match[4]
 			C := match[5]
@@ -39,7 +46,20 @@ func main(){
 			U := match[10]
 			Q := match[11]
 			X := match[12]
-			prof := match[12]*/
+			prof := match[12]
+			sqlStmt := `INSERT INTO classes VALUES('`+deptClassNo+`', '`+dept+`', `+classNo+`,`+A+`, `+B+`, `+C+`, `+D+`, `+F+`, `+I+`, `+S+`, `+U+`, `+Q+`, `+X+`, '`+prof+`');`
+			fmt.Println(sqlStmt)
+			_, err = db.Exec(sqlStmt)
+			if err != nil {
+				if err.(sqlite3.Error).Code == 19 { //constraint failed, likely PRIMARY KEY
+					fmt.Println("CONSTRAINT FAILED")
+					if err.(sqlite3.Error).ExtendedCode == 1555 { //UPDATE instead of INSERT if PRIMARY KEY CONSTRAINT failure
+						fmt.Println("PRIMARY KEY FAILED")
+						//UPDATE HERE
+					}
+				}
+				log.Fatal(err)
+			}
 		}
 	}
 	if err != nil {
