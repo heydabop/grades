556c137e1047874d2b8d8401492648bd481858c1
heydabop <heydabop@gmail.com>
2014-11-05 10:36:55 -0600

    init; gitignore; grades file; regex for parsing grades from file

---
+++ addDB/addDB.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	_ "github.com/mattn/go-sqlite3"
+	_ "database/sql"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main(){
+	classLineRegex := regexp.MustCompile(`^([A-Z]{4})-(\d{3})-\d{3}\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\d+\s+[0-9.]+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\d+\s+([A-Za-z ]+)`)
+	if(len(os.Args) != 2){
+		fmt.Printf("Usage: %s <txt file>\n", os.Args[0])
+	}
+	txtFile, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	txtReader := bufio.NewReader(txtFile)
+	line := ""
+	for line, err = txtReader.ReadString('\n'); err == nil; line, err = txtReader.ReadString('\n') {
+		if match := classLineRegex.FindStringSubmatch(line); match != nil {
+			for i := 1; i < len(match); i++ {
+				fmt.Printf("%s ", match[i])
+			}
+			fmt.Println()
+			/*dept := match[1]
+			classNo := match[2]
+			A := match[3]
+			B := match[4]
+			C := match[5]
+			D := match[6]
+			F := match[7]
+			I := match[8]
+			S := match[9]
+			U := match[10]
+			Q := match[11]
+			X := match[12]
+			prof := match[12]*/
+		}
+	}
+	if err != nil {
+		log.Println(err)
+	}
+}
