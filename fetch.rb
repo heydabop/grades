d75d1dfcc37015ea6e429ccb207271b05852e4d1
heydabop <heydabop@gmail.com>
2014-11-05 18:53:43 -0600

    dont be bad at ruby

--- fetch.rb
+++ fetch.rb
@@ -3,17 +3,17 @@
 url = ARGV[0]
 
 filename = %r{/(grd.*?)\.pdf}.match(url)[1]
-puts 'wget #{url} -O #{filename}.pdf'
-system 'wget #{url} -O #{filename}.pdf'
+puts "wget #{url} -O #{filename}.pdf"
+system "wget #{url} -O #{filename}.pdf"
 
-puts 'pdftotext -layout #{filename}.pdf'
-system 'pdftotext -layout #{filename}.pdf'
+puts "pdftotext -layout #{filename}.pdf"
+system "pdftotext -layout #{filename}.pdf"
 
-puts 'rm #{filename}.pdf'
-system 'rm #{filename}.pdf'
+puts "rm #{filename}.pdf"
+system "rm #{filename}.pdf"
 
-puts 'addDB/addDB #{filename}.txt classes.db'
-system 'addDB/addDB #{filename}.txt classes.db'
+puts "addDB/addDB #{filename}.txt classes.db"
+system "addDB/addDB #{filename}.txt classes.db"
 
-puts 'mv #{filename}.txt gradeFiles'
-system 'mv #{filename}.txt gradeFiles'
+puts "mv #{filename}.txt gradeFiles"
+system "mv #{filename}.txt gradeFiles"
