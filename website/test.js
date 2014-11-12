35ba40d31cfd90e2ff72e3776f3068a53e6c4690
heydabop <heydabop@gmail.com>
2014-11-12 15:17:24 -0600

    get rid of select, comment out logging

--- website/test.js
+++ website/test.js
@@ -6,92 +6,80 @@ function onlyUnique(e, i, self){
     return self.indexOf(e) === i;
 }
 
-document.addEventListener("DOMContentLoaded", function(event){
-
-    var deptSel = document.getElementById("deptSelect");
-    var o1 = document.createElement("option");
-
-    o1.value = "1";
-    o1.text = "Option Value 1";
-
-    deptSel.add(o1);
-
-    $(document).on('submit', '#classForm', function(e){
-        $.post($(this).attr('action'),
-               $(this).serialize(),
-               function(data){
-                   console.log(data);
-                   var classJson = $.parseJSON(data);
-                   if (typeof classJson.classes === 'undefined'){
-                       return;
+$(document).on('submit', '#classForm', function(e){
+    $.post($(this).attr('action'),
+           $(this).serialize(),
+           function(data){
+               //console.log(data);
+               var classJson = $.parseJSON(data);
+               if (typeof classJson.classes === 'undefined'){
+                   return;
+               }
+               var classArray = classJson.classes;
+               //console.log(classArray);
+               //var divTest = document.getElementById("testBox");
+               //divTest.innerHTML = data;
+               var i = 0;
+               var cols = [];
+               cols.push("Semester");
+               for(i = 0; i < classArray.length; ++i){
+                   cols.push(classArray[i].prof);
+               }
+               var colsUnique = cols.filter(onlyUnique);
+               var graphArray = [];
+               graphArray.push(colsUnique);
+               var colsMap = new Map();
+               for(i = 0; i < colsUnique.length; ++i){
+                   colsMap.set(colsUnique[i], i);
+               }
+               var rowsMap = new Map();
+               for(i = 0; i < classArray.length; ++i){
+                   if (typeof classArray[i].gpa === 'undefined'){
+                       continue;
                    }
-                   var classArray = classJson.classes;
-                   console.log(classArray);
-                   //var divTest = document.getElementById("testBox");
-                   //divTest.innerHTML = data;
-                   var i = 0;
-                   var cols = [];
-                   cols.push("Semester");
-                   for(i = 0; i < classArray.length; ++i){
-                       cols.push(classArray[i].prof);
-                   }
-                   var colsUnique = cols.filter(onlyUnique);
-                   var graphArray = [];
-                   graphArray.push(colsUnique);
-                   var colsMap = new Map();
-                   for(i = 0; i < colsUnique.length; ++i){
-                       colsMap.set(colsUnique[i], i);
-                   }
-                   var rowsMap = new Map();
-                   for(i = 0; i < classArray.length; ++i){
-                       if (typeof classArray[i].gpa === 'undefined'){
-                           continue;
-                       }
-                       var year = classArray[i].year;
-                       var sem = classArray[i].semester;
-                       var gpa = classArray[i].gpa;
-                       var prof = classArray[i].prof;
-                       var rowId = graphArray.length;
-                       if (typeof rowsMap.get(year + ' ' + sem) === 'undefined'){
-                           rowsMap.set(year + ' ' + sem, rowId)
-                           var newRow = new Array(colsUnique.length);
-                           var j = 0;
-                           newRow[0] = (year + ' ' + sem);
-                           graphArray.push(newRow);
-                       } else {
-                           rowId = rowsMap.get(year + ' ' + sem);
-                       }
-                       graphArray[rowId][colsMap.get(prof)] = parseFloat(gpa);
-                   }
-                   console.log(graphArray);
-
-                   var graphOptions = {
-                       vAxis: {
-                           title: 'GPA',
-                           gridlines: {
-                               count: -1
-                           }
-                       },
-                       hAxis: {
-                           title: 'Semester',
-                           gridlines: {
-                               count: -1
-                           }
-                       },
-                       pointSize: 5,
-                       interpolateNulls: true
-                   };
-
-                   if(chartLoaded){
-                       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-                       chart.draw(google.visualization.arrayToDataTable(graphArray), graphOptions);
+                   var year = classArray[i].year;
+                   var sem = classArray[i].semester;
+                   var gpa = classArray[i].gpa;
+                   var prof = classArray[i].prof;
+                   var rowId = graphArray.length;
+                   if (typeof rowsMap.get(year + ' ' + sem) === 'undefined'){
+                       rowsMap.set(year + ' ' + sem, rowId)
+                       var newRow = new Array(colsUnique.length);
+                       var j = 0;
+                       newRow[0] = (year + ' ' + sem);
+                       graphArray.push(newRow);
                    } else {
-                       console.log('chart not loaded');
+                       rowId = rowsMap.get(year + ' ' + sem);
                    }
-               });
-        e.preventDefault();
-    });
+                   graphArray[rowId][colsMap.get(prof)] = parseFloat(gpa);
+               }
+               //console.log(graphArray);
 
+               var graphOptions = {
+                   vAxis: {
+                       title: 'GPA',
+                       gridlines: {
+                           count: -1
+                       }
+                   },
+                   hAxis: {
+                       title: 'Semester',
+                       gridlines: {
+                           count: -1
+                       }
+                   },
+                   pointSize: 5,
+                   interpolateNulls: true
+               };
+
+               if(chartLoaded){
+                   var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                   chart.draw(google.visualization.arrayToDataTable(graphArray), graphOptions);
+               } else {
+                   //console.log('chart not loaded');
+               }
+           });
+    e.preventDefault();
 });
 
 google.setOnLoadCallback(function(){
