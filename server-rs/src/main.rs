370ca0c6f6fdf10a82cc8dbf8272dc13c4e0cec7
heydabop <heydabop@gmail.com>
2020-10-11 12:16:15 -0500

    move to rocket 0.5-dev

--- server-rs/src/main.rs
+++ server-rs/src/main.rs
@@ -1,13 +1,11 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
-#[macro_use]
 extern crate rocket_contrib;
 
 use rocket::http::Status;
 use rocket::request::{FromForm, LenientForm};
-use rocket_contrib::databases::rusqlite::{self, types::ToSql};
+use rocket_contrib::databases::database;
+use rocket_contrib::databases::rusqlite::{self, NO_PARAMS};
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
@@ -61,8 +59,15 @@ struct GradesResp {
 }
 
 #[get("/health")]
-fn health(db: ClassesDb) -> Status {
-    match db.query_row("SELECT 1 FROM classes", &[], |row| row.get::<usize, i32>(0)) {
+async fn health(db: ClassesDb) -> Status {
+    match db
+        .run(|c| {
+            c.query_row("SELECT 1 FROM classes", NO_PARAMS, |row| {
+                row.get::<usize, i32>(0)
+            })
+        })
+        .await
+    {
         Ok(one) => {
             if one == 1 {
                 Status::Ok
@@ -79,70 +84,75 @@ fn health(db: ClassesDb) -> Status {
 }
 
 #[post("/getData", data = "<body>")]
-fn get_data(db: ClassesDb, body: LenientForm<GradesReq>) -> Result<Json<GradesResp>, Status> {
+async fn get_data(db: ClassesDb, body: LenientForm<GradesReq>) -> Result<Json<GradesResp>, Status> {
     if body.dept.is_none() || body.number.is_none() {
         println!("Incomplete request");
         return Err(Status::BadRequest);
     }
     let mut wheres = Vec::with_capacity(6);
-    let mut params: Vec<&dyn ToSql> = Vec::with_capacity(6);
+    let mut params = Vec::with_capacity(6);
     if let Some(dept) = &body.dept {
         wheres.push("AND dept = ?");
-        params.push(dept);
+        params.push(dept.clone());
     }
     if let Some(number) = &body.number {
         wheres.push("AND number = ?");
-        params.push(number);
+        params.push(number.clone());
     }
     if let Some(section) = &body.section {
         wheres.push("AND section = ?");
-        params.push(section);
+        params.push(section.clone());
     }
     if let Some(prof) = &body.prof {
         wheres.push("AND prof = ?");
-        params.push(prof);
+        params.push(prof.clone());
     }
     if let Some(year) = &body.year {
         wheres.push("AND year = ?");
-        params.push(year);
+        params.push(year.clone());
     }
     if let Some(semester) = &body.semester {
         wheres.push("AND semester = ?");
-        params.push(semester);
+        params.push(semester.clone());
     }
-    let mut statement = db.prepare(&format!(r#"SELECT dept, number, section, a, b, c, d, f, i, s, u, q, x, prof, year, semester, gpa FROM classes WHERE 1 {} ORDER BY year ASC, CASE WHEN semester = 'SPRING' THEN 1 WHEN semester = 'SUMMER' THEN 2 ELSE 3 END ASC"#, &wheres.concat())).unwrap();
-    let mut rows = statement.query(&params[..]).unwrap();
+    let classes = db.run(move |c| {
+        let mut statement = c.prepare(&format!(r#"SELECT dept, number, section, a, b, c, d, f, i, s, u, q, x, prof, year, semester, gpa FROM classes WHERE 1 {} ORDER BY year ASC, CASE WHEN semester = 'SPRING' THEN 1 WHEN semester = 'SUMMER' THEN 2 ELSE 3 END ASC"#, &wheres.concat())).unwrap();
+        let rows = statement.query_map(&params[..], |row| {
+            Ok(Class {
+                dept: row.get_unwrap(0),
+                number: row.get_unwrap::<usize, i64>(1).to_string(),
+                section: row.get_unwrap::<usize, i64>(2).to_string(),
+                a: row.get_unwrap::<usize, i64>(3).to_string(),
+                b: row.get_unwrap::<usize, i64>(4).to_string(),
+                c: row.get_unwrap::<usize, i64>(5).to_string(),
+                d: row.get_unwrap::<usize, i64>(6).to_string(),
+                f: row.get_unwrap::<usize, i64>(7).to_string(),
+                i: row.get_unwrap::<usize, i64>(8).to_string(),
+                s: row.get_unwrap::<usize, i64>(9).to_string(),
+                u: row.get_unwrap::<usize, i64>(10).to_string(),
+                q: row.get_unwrap::<usize, i64>(11).to_string(),
+                x: row.get_unwrap::<usize, i64>(12).to_string(),
+                prof: row.get_unwrap(13),
+                year: row.get_unwrap::<usize, i64>(14).to_string(),
+                semester: row.get_unwrap(15),
+                gpa: row.get_unwrap::<usize, f64>(16).to_string(),
+            })
+        }).unwrap();
 
-    let mut classes = vec![];
-    while let Some(row) = rows.next() {
-        let row = row.unwrap();
-        classes.push(Class {
-            dept: row.get(0),
-            number: row.get::<usize, i64>(1).to_string(),
-            section: row.get::<usize, i64>(2).to_string(),
-            a: row.get::<usize, i64>(3).to_string(),
-            b: row.get::<usize, i64>(4).to_string(),
-            c: row.get::<usize, i64>(5).to_string(),
-            d: row.get::<usize, i64>(6).to_string(),
-            f: row.get::<usize, i64>(7).to_string(),
-            i: row.get::<usize, i64>(8).to_string(),
-            s: row.get::<usize, i64>(9).to_string(),
-            u: row.get::<usize, i64>(10).to_string(),
-            q: row.get::<usize, i64>(11).to_string(),
-            x: row.get::<usize, i64>(12).to_string(),
-            prof: row.get(13),
-            year: row.get::<usize, i64>(14).to_string(),
-            semester: row.get(15),
-            gpa: row.get::<usize, f64>(16).to_string(),
-        });
-    }
+        let mut classes = vec![];
+        for row in rows {
+            classes.push(row.unwrap());
+        }
 
-    return Ok(Json(GradesResp { classes: classes }));
+        classes
+    }).await;
+
+    Ok(Json(GradesResp { classes }))
 }
 
-fn main() {
+#[launch]
+fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .attach(ClassesDb::fairing())
         .mount("/", routes![health, get_data])
-        .launch();
 }
